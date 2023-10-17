70dbd4162416eba2644c70be47f7be80abaf9842
Luis <112034357+Luisf66@users.noreply.github.com>
2023-10-17 13:58:39 -0300

    Create index.js

---
+++ next1/index.js
@@ -0,0 +1,22 @@
+export default function Home() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Coluna 1</th>
+                    <th>Coluna 2</th>
+                    <th>Coluna 3</th>
+                    <th>Coluna 4</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Info 1</td>
+                    <td>Info 2</td>
+                    <td>Info 3</td>
+                    <td>Info 4</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
